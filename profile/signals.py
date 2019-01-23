68faeb671166e1a24460ba1c4376f9aa23b57858
Matthew Pava <matthew.pava@gmail.com>
2019-01-22 23:31:54 -0600

    adjusted signal

--- profile/signals.py
+++ profile/signals.py
@@ -7,19 +7,22 @@ from profile.models import Student, Payment
 
 
 @receiver(post_save, sender=Student)
-def post_save_student(sender, instance, **kwargs):
-    message = get_template('email.html').render({'student': instance})
-    send_mail(
-        "[Ocean Ink] New Student", message, "mouser@oceanink.net", ["matthew.pava@gmail.com"], True,
-        html_message=message
-    )
+def post_save_student(sender, instance, created, **kwargs):
+    if created:
+        message = get_template('email.html').render({'student': instance})
+        send_mail(
+            "[Ocean Ink] New Student", message, "mouser@oceanink.net",
+            ["matthew.pava@gmail.com", "drlepava@gmail.com"], True,
+            html_message=message
+        )
 
 
 @receiver(post_save, sender=Payment)
 def post_save_payment(sender, instance, **kwargs):
     message = get_template('email_receipt.html').render({'payment': instance})
     send_mail(
-        "[Ocean Ink] New Payment", message, "mouser@oceanink.net", ["matthew.pava@gmail.com"], True,
+        "[Ocean Ink] New Payment", message, "mouser@oceanink.net",
+        ["matthew.pava@gmail.com", "drlepava@gmail.com"], True,
         html_message=message
     )
 
