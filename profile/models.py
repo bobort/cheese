15f777634c0ad9318d93d984de1ebfdd7887e2b2
Matthew Pava <matthew.pava@gmail.com>
2019-03-20 02:01:06 -0500

    course should be a product

--- profile/models.py
+++ profile/models.py
@@ -176,6 +176,7 @@ class Product(models.Model):
 
 
 class Course(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     date_start = models.DateField()
     duration = models.DurationField()
     duration_access = models.DurationField()
