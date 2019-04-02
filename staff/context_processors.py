9fdc049696498741d65477878ebfb3fcd28432e8
Matthew Pava <matthew.pava@gmail.com>
2019-04-01 23:22:39 -0500

    don't flood the front page with occurrences that have already been filled out

--- staff/context_processors.py
+++ staff/context_processors.py
@@ -7,4 +7,4 @@ def get_event_occurrences_today(request):
     occurrences_to_save = Day(Event.objects.all(), timezone.now()).get_occurrences()
     for occurrence in occurrences_to_save:
         occurrence.save()
-    return {'occurrences': occurrences_to_save, 'now': timezone.now()}
+    return {'occurrences': occurrences_to_save.filter(groupsession__isnull=True), 'now': timezone.now()}
