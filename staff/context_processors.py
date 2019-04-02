a9b5e0dcec2695ff0d18fc561e1ea1e5a64a31a5
Matthew Pava <matthew.pava@gmail.com>
2019-04-02 07:40:36 -0500

    issue occurrences

--- staff/context_processors.py
+++ staff/context_processors.py
@@ -5,10 +5,10 @@ from schedule.periods import Day
 
 def get_event_occurrences_today(request):
     occurrences_to_save = Day(Event.objects.all(), timezone.now()).get_occurrences()
-    occurrences_to_save = Occurrence.objects.filter(
-        pk__in=[o.pk for o in occurrences_to_save],
-        groupsession__isnull=True
-    )
+    # occurrences_to_save = Occurrence.objects.filter(
+    #     pk__in=[o.pk for o in occurrences_to_save],
+    #     groupsession__isnull=True
+    # )
     for occurrence in occurrences_to_save:
         occurrence.save()
     return {
