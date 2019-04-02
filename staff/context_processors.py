db1fe54104194f932e9d4b7ddb4f52c9d66f7a9a
Matthew Pava <matthew.pava@gmail.com>
2019-04-01 23:32:10 -0500

    Occurrences working now

--- staff/context_processors.py
+++ staff/context_processors.py
@@ -5,6 +5,13 @@ from schedule.periods import Day
 
 def get_event_occurrences_today(request):
     occurrences_to_save = Day(Event.objects.all(), timezone.now()).get_occurrences()
+    occurrences_to_save = Occurrence.objects.filter(
+        pk__in=[o.pk for o in occurrences_to_save],
+        groupsession__isnull=True
+    )
     for occurrence in occurrences_to_save:
         occurrence.save()
-    return {'occurrences': Occurrence.objects.filter(pk__in=occurrences_to_save, roupsession__isnull=True), 'now': timezone.now()}
+    return {
+        'occurrences': occurrences_to_save,
+        'now': timezone.now()
+    }
