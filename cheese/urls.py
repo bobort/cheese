3df642e0eade7d2b72c86109aa23a640158a4480
Matthew Pava <4dbobort@msn.com>
2019-06-18 00:04:39 -0500

    improved TinyMCE support

--- cheese/urls.py
+++ cheese/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^tinymce/', include('tinymce.urls')),
     path('', include('frontend.urls')),
     path('accounts/', include('allauth.urls')),
     path('profile/', include('profile.urls')),
