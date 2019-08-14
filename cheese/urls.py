8ab779ecf256fb01107c0b3f405af945c49158f1
Matthew Pava <4dbobort@msn.com>
2019-08-13 20:06:30 -0500

    full calendar changes

--- cheese/urls.py
+++ cheese/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schedule/', include('schedule.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace="api-auth")),
     url(r'^report_builder/', include('report_builder.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
--- profile/urls.py
+++ profile/urls.py
@@ -6,6 +6,8 @@ app_name = 'profile'
 
 urlpatterns = [
     path('pay', views.process_payment, name='pay'),
+    path('calendar', views.CalendarView.as_view(), name='calendar'),
+    path('api/occurrences', views.api_occurrences, name='api-occurrences'),
     path('receipt/<int:pk>', views.Receipt.as_view(), name='receipt'),
     path('update/<int:pk>', views.ProfileUpdate.as_view(), name='update'),
     path('<int:pk>', views.ProfileView.as_view(), name='view'),
